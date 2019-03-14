ba6cd4bd5a29463b8a93d6b3c371923d8bac0c0f
UramnOIL <uramnoil@outlook.jp>
2019-03-14 13:11:43 +0900

    修正

--- src/main/kotlin/com/uramnoil/gomint/helloworld/HelloWorldPlugin.kt
+++ src/main/kotlin/com/uramnoil/gomint/helloworld/HelloWorldPlugin.kt
@@ -1,19 +1,17 @@
 package com.uramnoil.gomint.helloworld
 
-import com.eclipsesource.json.Json
 import io.gomint.config.InvalidConfigurationException
+import io.gomint.event.EventHandler
 import io.gomint.event.EventListener
 import io.gomint.event.player.PlayerJoinEvent
 import io.gomint.plugin.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import okhttp3.Request
 
-@PluginName("HelloWorld")
-@Version(major = 1, minor = 0)
-@Startup(StartupPriority.LOAD)
+@PluginName("HelloWorld")    //プラグインの名前
+@Version(major = 1, minor = 0)      //バージョン
+@Startup(StartupPriority.LOAD)      //ロードのタイミング
 class HelloWorldPlugin: Plugin(), EventListener {
     companion object {
         const val WH_URL_BASE   = "http://weather.livedoor.com/forecast/webservice/json/v1?city=140010"
@@ -29,9 +27,10 @@ class HelloWorldPlugin: Plugin(), EventListener {
      */
     override fun onInstall() {
         logger.info( "たぶんonEnable" )
-        pluginManager.registerListener(this, this)
+        pluginManager.registerListener(this, this)  //イベントリスナの登録
     }
 
+    @EventHandler
     fun onJoin(event: PlayerJoinEvent) = GlobalScope.launch {
         async {
             return@async helloWorld.getHelloWorld()
