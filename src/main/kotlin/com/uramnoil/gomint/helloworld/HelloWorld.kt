da9b3eff3120d60473739aa7880e9bc1dcb301cf
UramnOIL <uramnoil@outlook.jp>
2019-03-14 12:34:59 +0900

    分けた

--- src/main/kotlin/com/uramnoil/gomint/helloworld/HelloWorld.kt
+++ src/main/kotlin/com/uramnoil/gomint/helloworld/HelloWorld.kt
@@ -1,5 +1,7 @@
 package com.uramnoil.gomint.helloworld
 
+import com.eclipsesource.json.Json
+import com.eclipsesource.json.JsonObject
 import io.gomint.config.InvalidConfigurationException
 import io.gomint.event.EventListener
 import io.gomint.event.player.PlayerJoinEvent
@@ -14,6 +16,11 @@ import okhttp3.Request
 @Version(major = 1, minor = 0)
 @Startup(StartupPriority.LOAD)
 class HelloWorld : Plugin(), EventListener {
+    companion object {
+        const val WH_URL_BASE   = "http://weather.livedoor.com/forecast/webservice/json/v1?city=140010"
+        const val WH_URL_PARAMS = "city=14410"
+    }
+
     override fun onStartup() {
         logger.info( "たぶんonLoad" )
     }
@@ -22,17 +29,24 @@ class HelloWorld : Plugin(), EventListener {
      * @throws InvalidConfigurationException
      */
     override fun onInstall() {
-        logger.info( "たぶんonLoad" )
+        logger.info( "たぶんonEnable" )
+        pluginManager.registerListener(this, this)
     }
 
     fun onJoin(event: PlayerJoinEvent) = GlobalScope.launch {
-        val client = OkHttpClient()
-        val request = Request.Builder().url("http://weather.livedoor.com/forecast/webservice/json/v1?city=140010").build()  //横浜市の天気！！！一度はおいで！！！！
         async {
-            val response = client.newCall(request).execute()
-            return@async response.body()?.toString()
+            return@async getWeather()
         }.await().let {
             event.player.sendMessage(it)
         }
+    }
+
+    fun getWeather(): String {
+        val client = OkHttpClient()
+        val request = Request.Builder().url(WH_URL_BASE + "?" + WH_URL_PARAMS).build()  //横浜市の天気！！！一度はおいで！！！！
+        val response = client.newCall(request).execute()
+        val body = response.body().toString()
+        val json = Json.parse(body).asObject()
+        return json["description"].asObject()["text"].toString()
     }
 }
